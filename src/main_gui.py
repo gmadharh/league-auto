54fc20684d147085f1dd2b4d84761829946b183c
Gevin Madharha <gevinmadharha@gmail.com>
2020-04-21 19:28:09 -0400

    almost got it working, got to fix a little bit more

--- src/main_gui.py
+++ src/main_gui.py
@@ -29,7 +29,7 @@ class LeagueGui:
 		self.start_button = Button(master,text="Start",command=self.start_script)
 		self.start_button.grid(row=2,column=0)
 
-		self.stop_button = Button(master,text="Stop")
+		self.stop_button = Button(master,text="Stop",command=self.stop_script)
 		self.stop_button.grid(row=2,column=1)
 
 	def num_input(self):
@@ -57,7 +57,12 @@ class LeagueGui:
 			except:
 				pass
 
-		self.master.after(1000,self.check_in_queue)
+			self.master.after(1000,self.check_in_queue)
+		else:
+			self.in_queue()
+
+
+		
 
 	def in_queue(self):
 
@@ -71,20 +76,27 @@ class LeagueGui:
 			print('in queue')
 
 			queue_label = pyautogui.locateOnScreen('../res/queue.png',confidence=0.8)
+			self.master.after(1000,self.in_queue)
 
-		self.master.after(1000,self.in_queue)
+		
+		self.accept_queue()
+
+		
 
 	def accept_queue(self):
 
 		global accept_button
 
 		# keep searching for accept button
-		if accept_button is None:
+		if accept_button is None and running:
 			print('searching for accept button')
 			try:
 				accept_button = pyautogui.locateOnScreen('../res/accept-queue.png',confidence=0.8)
 			except:
 				pass
+
+			self.master.after(1000,self.accept_queue)
+
 		else:	
 			print('found accept_button')
 			# get the location of the accept button
@@ -94,13 +106,17 @@ class LeagueGui:
 			# click it
 			pyautogui.click(accept_location)
 
-		self.master.after(1000,self.accept_queue)
+			self.champ_role()
+
+		
+
+		
 
 	def champ_role(self):
 
 		global chat_box
 
-		if chat_box is None:
+		if chat_box is None and running:
 
 			print('searching for chat bot')
 
@@ -108,6 +124,8 @@ class LeagueGui:
 				chat_box = pyautogui.locateOnScreen('../res/chat-box.png',confidence=0.8)
 			except:
 				pass
+
+			self.master.after(1000,self.champ_role)
 
 		else:	
 			print('found it')
@@ -119,24 +137,27 @@ class LeagueGui:
 			print('spamming role' )
 
 			for i in range(self.num_input()):
-				pyautogui.write('top')
+				pyautogui.write(self.role_entry.get())
 				pyautogui.press('enter')
 
-		self.master.after(1000,self.champ_role)
+		
+
+		
 
 
 	def start_script(self):
 
 		global running
 		running = True
+
 		self.check_in_queue()
-		self.in_queue()
-		self.accept_queue()
-		self.champ_role()
+		
 
+	def stop_script(self):
 
-
-
+		global running
+		running = False
+		self.master.destroy()
 
 
 
